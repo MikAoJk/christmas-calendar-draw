ef79f75f42c473218e1b891770b4c1da2084ac70
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2022-11-07 08:19:41 +0100

    moved out props

--- src/components/WheelOfFortune/Wheel/Wheel.tsx
+++ src/components/WheelOfFortune/Wheel/Wheel.tsx
@@ -1,21 +1,19 @@
 import React, {useEffect, useState} from 'react'
+import {Participants} from "../WheelOfFortune";
+import styles from "../../../styles/Home.module.css";
 
 
 interface WheelProps {
-    participants: string[],
-    segColors: string[],
-    winningSegment: any,
-    primaryColor: string,
-    contrastColor: string,
-    size: number,
-    upDuration: number,
-    downDuration: number,
-    fontFamily: string,
+    participants: Participants[],
     onFinished: (winner: string) => void;
 }
 
 
 const Wheel = (wheelProps: WheelProps) => {
+    const contrastColor = 'white'
+    const primaryColor = 'black'
+    const fontFamily = 'proxima-nova'
+    const size = 290
     let currentParticipant = ''
     let isStarted = false
     const [isFinished, setFinished] = useState(false)
@@ -25,8 +23,8 @@ const Wheel = (wheelProps: WheelProps) => {
     let angleDelta = 0
     let canvasContext: any = null
     let maxSpeed = Math.PI / wheelProps.participants.length
-    const upTime = wheelProps.participants.length * wheelProps.upDuration
-    const downTime = wheelProps.participants.length * wheelProps.downDuration
+    const upTime = wheelProps.participants.length * 100
+    const downTime = wheelProps.participants.length * 1000
     let spinStart = 0
     let frames = 0
     const centerX = 300
@@ -46,26 +44,27 @@ const Wheel = (wheelProps: WheelProps) => {
         let canvas = document.getElementById('canvas')
         console.log(navigator)
         if (navigator.userAgent.indexOf('MSIE') !== -1) {
-            canvas = document.createElement('canvas') as HTMLCanvasElement
-            canvas.setAttribute('width', String(1000))
-            canvas.setAttribute('height', String(600))
-            canvas.setAttribute('id', 'canvas')
-            // TODO
+            canvas = document.createElement('canvas') as HTMLCanvasElement | null;
+            canvas?.setAttribute('width', String(1000))
+            canvas?.setAttribute('height', String(600))
+            canvas?.setAttribute('id', 'canvas')
             // @ts-ignore
-            document.getElementById('wheel').appendChild(canvas)
+            document?.getElementById('wheel')?.appendChild(canvas)
         }
-        // TODO
+        canvas!.addEventListener('click', spin, false)
         // @ts-ignore
-        canvas.addEventListener('click', spin, false)
-        // TODO
-        // @ts-ignore
-        canvasContext = canvas.getContext('2d')
+        canvasContext = canvas?.getContext('2d')
     }
+
+    const getRandomIntInclusive = () => {
+        return Math.floor(Math.random() * (8 - 1 + 1) + 1)
+    }
+
     const spin = () => {
         isStarted = true
         if (timerHandle === 0) {
             spinStart = new Date().getTime()
-            maxSpeed = Math.PI / wheelProps.participants.length
+            maxSpeed = (Math.PI + getRandomIntInclusive()) / wheelProps.participants.length
             frames = 0
             timerHandle = setInterval(onTimerTick, timerDelay)
         }
@@ -80,21 +79,8 @@ const Wheel = (wheelProps: WheelProps) => {
             progress = duration / upTime
             angleDelta = maxSpeed * Math.sin((progress * Math.PI) / 2)
         } else {
-            if (wheelProps.winningSegment) {
-                if (currentParticipant === wheelProps.winningSegment && frames > wheelProps.participants.length) {
-                    progress = duration / upTime
-                    angleDelta =
-                        maxSpeed * Math.sin((progress * Math.PI) / 2 + Math.PI / 2)
-                    progress = 1
-                } else {
-                    progress = duration / downTime
-                    angleDelta =
-                        maxSpeed * Math.sin((progress * Math.PI) / 2 + Math.PI / 2)
-                }
-            } else {
                 progress = duration / downTime
                 angleDelta = maxSpeed * Math.sin((progress * Math.PI) / 2 + Math.PI / 2)
-            }
             if (progress >= 1) finished = true
         }
 
@@ -123,22 +109,22 @@ const Wheel = (wheelProps: WheelProps) => {
 
     const drawSegment = (key: number, lastAngle: number, angle: number) => {
         const ctx = canvasContext
-        const value = wheelProps.participants[key]
+        const value = wheelProps.participants[key].name
         ctx.save()
         ctx.beginPath()
         ctx.moveTo(centerX, centerY)
-        ctx.arc(centerX, centerY, wheelProps.size, lastAngle, angle, false)
+        ctx.arc(centerX, centerY, size, lastAngle, angle, false)
         ctx.lineTo(centerX, centerY)
         ctx.closePath()
-        ctx.fillStyle = wheelProps.segColors[key]
+        ctx.fillStyle = wheelProps.participants[key].color
         ctx.fill()
         ctx.stroke()
         ctx.save()
         ctx.translate(centerX, centerY)
         ctx.rotate((lastAngle + angle) / 2)
-        ctx.fillStyle = wheelProps.contrastColor
-        ctx.font = 'bold 1em ' + wheelProps.fontFamily
-        ctx.fillText(value.substring(0, 21), wheelProps.size / 2 + 20, 0)
+        ctx.fillStyle = contrastColor
+        ctx.font = 'bold 1em ' + fontFamily
+        ctx.fillText(value.substring(0, 21), size / 2 + 20, 0)
         ctx.restore()
     }
 
@@ -148,10 +134,10 @@ const Wheel = (wheelProps: WheelProps) => {
         const len = wheelProps.participants.length
         const PI2 = Math.PI * 2
         ctx.lineWidth = 1
-        ctx.strokeStyle = wheelProps.primaryColor
+        ctx.strokeStyle = primaryColor
         ctx.textBaseline = 'middle'
         ctx.textAlign = 'center'
-        ctx.font = '1em ' + wheelProps.fontFamily
+        ctx.font = '1em ' + fontFamily
         for (let i = 1; i <= len; i++) {
             const angle = PI2 * (i / len) + angleCurrent
             drawSegment(i - 1, lastAngle, angle)
@@ -162,31 +148,31 @@ const Wheel = (wheelProps: WheelProps) => {
         ctx.beginPath()
         ctx.arc(centerX, centerY, 50, 0, PI2, false)
         ctx.closePath()
-        ctx.fillStyle = wheelProps.primaryColor
+        ctx.fillStyle = primaryColor
         ctx.lineWidth = 10
-        ctx.strokeStyle = wheelProps.contrastColor
+        ctx.strokeStyle = contrastColor
         ctx.fill()
-        ctx.font = 'bold 1em ' + wheelProps.fontFamily
-        ctx.fillStyle = wheelProps.contrastColor
+        ctx.font = 'bold 1em ' + fontFamily
+        ctx.fillStyle = contrastColor
         ctx.textAlign = 'center'
         ctx.fillText('spin', centerX, centerY + 3)
         ctx.stroke()
 
         // Draw outer circle
         ctx.beginPath()
-        ctx.arc(centerX, centerY, wheelProps.size, 0, PI2, false)
+        ctx.arc(centerX, centerY, size, 0, PI2, false)
         ctx.closePath()
 
         ctx.lineWidth = 10
-        ctx.strokeStyle = wheelProps.primaryColor
+        ctx.strokeStyle = primaryColor
         ctx.stroke()
     }
 
     const drawNeedle = () => {
         const ctx = canvasContext
         ctx.lineWidth = 1
-        ctx.strokeStyle = wheelProps.contrastColor
-        ctx.fileStyle = wheelProps.contrastColor
+        ctx.strokeStyle = contrastColor
+        ctx.fileStyle = contrastColor
         ctx.beginPath()
         ctx.moveTo(centerX + 20, centerY - 50)
         ctx.lineTo(centerX - 20, centerY - 50)
@@ -201,16 +187,16 @@ const Wheel = (wheelProps: WheelProps) => {
         if (i < 0) i = i + wheelProps.participants.length
         ctx.textAlign = 'center'
         ctx.textBaseline = 'middle'
-        ctx.fillStyle = wheelProps.primaryColor
-        ctx.font = 'bold 1.5em ' + wheelProps.fontFamily
-        currentParticipant = wheelProps.participants[i]
-        isStarted && ctx.fillText(currentParticipant, centerX + 10, centerY + wheelProps.size + 50)
+        ctx.fillStyle = primaryColor
+        ctx.font = 'bold 1.5em ' + fontFamily
+        currentParticipant = wheelProps.participants[i].name
+        isStarted && ctx.fillText(currentParticipant, centerX + 10, centerY + size + 50)
     }
     const clear = () => {
         canvasContext.clearRect(0, 0, 1000, 800)
     }
     return (
-        <div id='wheel'>
+        <div id='wheel' className={styles.main}>
             <canvas
                 id='canvas'
                 width='1000'
--- src/components/WheelOfFortune/WheelOfFortune.tsx
+++ src/components/WheelOfFortune/WheelOfFortune.tsx
@@ -1,6 +1,6 @@
 import styles from "../../styles/Home.module.css";
 
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import Wheel from "./Wheel/Wheel";
 import PrevWinners from "../PrevWinners/PrevWinners";
 
@@ -13,54 +13,71 @@ const WheelOfFortune = () => {
         setWinner(participant)
     }
 
-    const participants = [
-        'per',
-        'ola',
-        'jonny',
-        'pål',
-        'alf',
-        'viggo',
-        'trong',
-        'espen',
-        'trond',
-        'hans',
-        'vegard',
-        'joakim',
-        'andre',
-        'øyvind',
-        'ronny'
-    ]
-
-    // Should match the number of participants
-    const segColors = [
-        '#EE4040',
-        '#F0CF50',
-        '#815CD1',
-        '#3DA5E0',
-        '#34A24F',
-        '#F9AA1F',
-        '#EC3F3F',
-        '#FF9000',
-        '#F0CF50',
-        '#815CD1',
-        '#3DA5E0',
-        '#34A24F',
-        '#F9AA1F',
-        '#EC3F3F',
-        '#FF9000'
-    ]
+    const participants: Participants[] =
+        [{
+            name: 'per',
+            color: '#EE4040'
+        },
+            {
+                name: 'ola',
+                color: '#F0CF50'
+            },
+            {
+                name: 'jonny',
+                color: '#815CD1'
+            },
+            {
+                name: 'pål',
+                color: '#3DA5E0'
+            },
+            {
+                name: 'viggo',
+                color: '#F0CF50'
+            },
+            {
+                name: 'espen',
+                color: '#EE4040'
+            },
+            {
+                name: 'trond',
+                color: '#F0CF50'
+            },
+            {
+                name: 'hans',
+                color: '#815CD1'
+            },
+            {
+                name: 'vegard',
+                color: '#3DA5E0'
+            },
+            {
+                name: 'joakim',
+                color: '#34A24F'
+            },
+            {
+                name: 'øyvind',
+                color: '#EC3F3F'
+            },
+            {
+                name: 'ronny',
+                color: '#FF9000'
+            }
+        ]
 
     return (
         <div className={styles.container}>
             <PrevWinners/>
             {winner && <h3>Todays winner is: {winner}</h3>}
-            <Wheel contrastColor={'white'} downDuration={1000}
-                   fontFamily={'proxima-nova'}
-                   primaryColor={'black'} segColors={segColors} participants={participants} size={290}
-                   upDuration={100} winningSegment={1000} onFinished={winnerHandler}/>
+            <Wheel participants={participants}
+                   onFinished={winnerHandler}/>
         </div>
     )
 
+}
+
+export type Participants = {
+    name: string
+    color: string
 }
 
 
