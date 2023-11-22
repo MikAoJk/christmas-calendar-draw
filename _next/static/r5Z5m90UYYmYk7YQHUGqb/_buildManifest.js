324ed7f22692d742019a89952d4c009418969333
MikAoJk <joakimkartveit@gmail.com>
2023-11-22 14:06:43 +0000

    Deploying to gh-pages from @ MikAoJk/christmas-calendar-draw@5aa9e3fbfdf31014ffb84f84a080660d455dafbf 🚀

---
+++ _next/static/r5Z5m90UYYmYk7YQHUGqb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/2602b037fcee6b90.css","static/chunks/pages/index-77edfeab38f19a1c.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
