4bf4362111b4197a243507c508c055c53dd6fb5c
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-11-22 20:27:53 +0100

    Update PrevWinners.tsx

--- src/components/PrevWinners/PrevWinners.tsx
+++ src/components/PrevWinners/PrevWinners.tsx
@@ -3,7 +3,7 @@ import styles from "../../styles/Home.module.css";
 const PrevWinners = () => {
 
     return (
-        <div className={styles.center}>
+        <div className={styles.main}>
             <table>
                 <thead>
                 <tr>
